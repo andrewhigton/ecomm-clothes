575d93ef099d1a6d721a3ee69d20a1cb05f815c2
Andrew Higton <andrewhigton@outlook.com>
2019-10-03 09:47:21 +0100

    created smaller components for homepage/styled menu-item component

---
+++ src/page/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
+import './homepage.styles.scss';
+
+const HomePage = () => 
+//why these brackets?
+(
+	<div className='homepage'>
+		<Directory />
+	</div>
+)
+
+export default HomePage;
