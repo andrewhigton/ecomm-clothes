2ad2e844efbe6c6e17cbfe37ab2632d7e25f419c
Andrew Higton <andrewhigton@outlook.com>
2019-10-09 21:47:34 +0100

    added signin and signup to firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,7 +13,34 @@ const config = {
     measurementId: "G-1EWXGR02TE"
   };
 
+
+
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
@@ -23,3 +50,37 @@ provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
+
+// export const createUserProfileDocument = async(userAuth, additionalData ) => {
+// 	if(!userAuth) return;
+
+// 	const userRef = firestore.doc(`users/${userAuth.uid}`);
+// 		// 'users/128sdfsdfrg')
+// 	const snapShot = await userRef.get(); 
+// 	if(!snapShot.exists) {
+// 		const { displayName, email } = userAuth;
+// 		const createdAt = new Date();	
+// 		try {
+// 			await userRef.set({
+// 				displayName,
+// 				email,
+// 				createdAt,
+// 				...additionalData
+// 			})
+// 		} catch(error) {
+// 			console.log('error creating user', error.message);
+// 		}
+// 	}	//console.log(snapShot);
+// return userRef;	
+// } 
+
+// firebase.initializeApp(config);
+
+// export const auth = firebase.auth();
+// export const firestore = firebase.firestore();
+
+// const provider = new firebase.auth.GoogleAuthProvider();
+// provider.setCustomParameters({ prompt: 'select_account' });
+// export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+// export default firebase;
